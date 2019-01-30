235e84e31aabad592579afb5e5e36756f39977da
Mpilo Mshengu <mmshengu@retrorebbit.co.za>
2019-01-30 11:13:59 +0200

    updated UnitTest

---
+++ Know-Your-Nation-Speedy/Know-Your-Nation-Speedy/KYNSDemo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Know_Your_Nation_Speedy
+{
+    public class KYNSDemo
+    {
+        public string SpeedysFriend(string name) {
+            return name;
+        }
+    }
+}
--- Know-Your-Nation-Speedy/NUnitTestKYNS/UnitTest.cs
+++ Know-Your-Nation-Speedy/NUnitTestKYNS/UnitTest.cs
@@ -1,18 +1,31 @@
+using Know_Your_Nation_Speedy;
 using NUnit.Framework;
 
 namespace Tests
 {
     public class Tests
     {
+        private KYNSDemo kyns;
+      
         [SetUp]
         public void Setup()
         {
+            kyns = new KYNSDemo();
         }
 
         [Test]
         public void Test1()
         {
             Assert.Pass();
+        }
+        [TestCase("Mpilo","Shakes")]
+        public void Test(string name,string outcome)
+        {
+            //assign
+            //act
+            var result = kyns.SpeedysFriend(name);
+            //assert
+            Assert.AreEqual(name, outcome);
         }
     }
 }
